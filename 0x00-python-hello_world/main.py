96fe1ec6aa6e1c20f60fbf9f04490561c9b55428
Grace195 <nanaakua55@gmail.com>
2023-11-27 17:36:50 -0800

    hello world

--- 0x00-python-hello_world/main.py
+++ 0x00-python-hello_world/main.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 print("Best School")
 
-guillaume@ubuntu:~/py/0x00$ export PYFILE=main.py
-guillaume@ubuntu:~/py/0x00$ ./101-compile
-Compiling main.py ...
-guillaume@ubuntu:~/py/0x00$ ls
-101-compile  main.py  main.pyc
