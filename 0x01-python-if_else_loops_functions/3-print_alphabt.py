053402d8eea9620b409010f9f5de4409de424167
Grace195 <nanaakua55@gmail.com>
2023-11-29 17:19:18 -0800

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,11 @@
-#!/usr/bin/python3
-for i in range(97, 123):
-    if (i == 101) or (i == 113):
-        continue
-    print(chr(i).format(), end="")
+#!/usr/bin/python
+# Author - Grace195
+for letter in range(97, 123):
+    if chr(letter) l= 'q' and chr(letter) l= 'e':
+        print("[]".format(chr)(letter)), end="")
+
+
+
+
+
+
